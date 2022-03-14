60bc01195aeeafbf71e2d6b293fa207313f4382a
Jeffrey <Jeffrey2971@outlook.com>
2022-03-14 20:09:15 +0800

    WeChat: 获取翻译相关结果 controller

---
+++ src/main/java/com/jeffrey/wechat/controller/GetResponseResultController.java
@@ -0,0 +1,14 @@
+package com.jeffrey.wechat.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jeffrey
+ * @since JDK 1.8
+ */
+
+@RestController
+@RequestMapping("/")
+public class GetResponseResultController {
+}
