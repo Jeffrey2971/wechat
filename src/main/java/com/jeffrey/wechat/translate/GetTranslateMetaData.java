e9a11b22ae468daba5ef848ad4469b72a38def9d
Jeffrey <Jeffrey2971@outlook.com>
2022-03-22 21:53:20 +0800

    WeChat：代码健壮性修改

--- src/main/java/com/jeffrey/wechat/translate/GetTranslateMetaData.java
+++ src/main/java/com/jeffrey/wechat/translate/GetTranslateMetaData.java
@@ -1,18 +1,21 @@
 package com.jeffrey.wechat.translate;
 
 import com.google.gson.Gson;
-import com.jeffrey.wechat.entity.Config;
+import com.jeffrey.wechat.config.WeChatAutoConfiguration;
 import com.jeffrey.wechat.entity.translation.TranslationData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
 import org.springframework.util.DigestUtils;
 import org.springframework.util.FileCopyUtils;
 
+
 import java.io.File;
+
 import java.io.IOException;
-import java.nio.file.Files;
-import java.util.Map;
+
+import java.util.*;
 
 /**
  * 根据给定的图片返回该图片中的数据信息
@@ -21,11 +24,16 @@ import java.util.Map;
 @Slf4j
 public class GetTranslateMetaData {
 
-    private static Config.BaiduTranslationConfig config;
+    private static WeChatAutoConfiguration.BaiduTranslationConfig config;
 
     private static HttpParams params;
 
-    private static final HttpClient httpClient = new HttpClient();
+    private static HttpClient httpClient;
+
+    @Autowired
+    public void setHttpClient(HttpClient httpClient) {
+        GetTranslateMetaData.httpClient = httpClient;
+    }
 
 
     @Autowired
@@ -34,7 +42,7 @@ public class GetTranslateMetaData {
     }
 
     @Autowired
-    private void setConfig(Config.BaiduTranslationConfig config) {
+    private void setConfig(WeChatAutoConfiguration.BaiduTranslationConfig config) {
         GetTranslateMetaData.config = config;
     }
 
@@ -45,16 +53,14 @@ public class GetTranslateMetaData {
      * 传递一个本地的图片地址，返回 TranslationData 对象
      * TranslationData 内部封装了关于图片中的信息
      */
-    public static TranslationData getData(String localPicUrlPath) {
+    public static TranslationData getData(File localPicUrlPath) {
 
-        final File image = new File(localPicUrlPath);
-
-        if (!image.exists() && !image.isFile()) {
-            log.error("给定的图片路径参数：{}，不是一个有效的本地图片路径", image.getAbsolutePath());
+        if (!localPicUrlPath.exists() && !localPicUrlPath.isFile()) {
+            log.error("给定的图片路径参数：{}，不是一个有效的本地图片路径", localPicUrlPath.getAbsolutePath());
             throw new RuntimeException("给定的文件路径不存在或它不是图片");
         }
 
-        params.put("image", image, config.getFileContentType());
+        params.put("image", localPicUrlPath, config.getFileContentType());
         params.put("from", config.getAuto());
         params.put("to", config.getZh());
         params.put("appid", config.getBaiduTransactionAppId());
@@ -70,23 +76,22 @@ public class GetTranslateMetaData {
             throw new RuntimeException("参数加密过程中出现了异常");
         }
 
+        log.info("开始解析图片内容");
         return new Gson().fromJson(httpClient.post(config.getReqUrl(), params), TranslationData.class);
     }
 
-    private static String sign(Config.BaiduTranslationConfig config, HttpParams params) throws IOException {
+    private static String sign(WeChatAutoConfiguration.BaiduTranslationConfig config, HttpParams params) throws IOException {
+
         if (config == null || params == null) {
             throw new RuntimeException("传递的参数 Config.BaiduTranslationConfig config 或 HttpParams params 不能为空");
         }
+
         Map<String, String> stringParams = params.getStringParams();
         Map<String, HttpParams.FileWrapper> fileParams = params.getFileParams();
 
 
         HttpParams.FileWrapper file = fileParams.get("image");
-
-
-//        String md5Image = DigestUtils.md5DigestAsHex(Files.readAllBytes(file.file.toPath()));
         String md5Image = DigestUtils.md5DigestAsHex(FileCopyUtils.copyToByteArray(file.file));
-
 
         return DigestUtils.md5DigestAsHex((config.getBaiduTransactionAppId() + md5Image + stringParams.get("salt") + stringParams.get("cuid") + stringParams.get("mac") + config.getBaiduTransactionAppKey()).getBytes()).toLowerCase();
     }
