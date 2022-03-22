981a4a71e001648f9a554891d568c8279c3a8a99
Jeffrey <Jeffrey2971@outlook.com>
2022-03-22 21:57:33 +0800

    WeChat：矩阵变量配置、拦截器配置

--- src/main/java/com/jeffrey/wechat/config/WeChatConfiguration.java
+++ src/main/java/com/jeffrey/wechat/config/WeChatConfiguration.java
@@ -1,10 +1,20 @@
 package com.jeffrey.wechat.config;
 
+import com.jeffrey.wechat.interceptor.BlackUserInterceptor;
 import com.jeffrey.wechat.interceptor.FollowInterceptor;
-import com.jeffrey.wechat.interceptor.FrequencyInterceptor;
+import com.jeffrey.wechat.interceptor.RequestInterceptor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.util.UrlPathHelper;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 /**
  * @author jeffrey
@@ -13,10 +23,26 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WeChatConfiguration implements WebMvcConfigurer {
+
+    /**
+     * 使矩阵变量生效
+     *
+     * @param configurer PathMatchConfigurer
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        UrlPathHelper urlPathHelper = new UrlPathHelper();
+        urlPathHelper.setRemoveSemicolonContent(false);
+        configurer.setUrlPathHelper(urlPathHelper);
+    }
+
+    @Bean
+    public HandlerInterceptor blackUserInterceptor(){
+        return new BlackUserInterceptor();
+    }
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new FollowInterceptor()).addPathPatterns("/**");
-        registry.addInterceptor(new FollowInterceptor()).addPathPatterns("/**");
-        registry.addInterceptor(new FrequencyInterceptor()).addPathPatterns("/**");
+        registry.addInterceptor(blackUserInterceptor()).addPathPatterns("/**");
     }
 }
