bb3035171350a4f8b492ac8db694b56aafeb5ffe
Jeffrey <Jeffrey2971@outlook.com>
2022-03-22 21:45:53 +0800

    WeChat：添加注释

--- src/main/java/com/jeffrey/wechat/service/WeChatService.java
+++ src/main/java/com/jeffrey/wechat/service/WeChatService.java
@@ -10,8 +10,21 @@ import java.util.Map;
 
 public interface WeChatService {
 
+    /**
+     * 微信接入校验
+     * @param timestamp 时间戳
+     * @param nonce nonce
+     * @param signature signature
+     * @param echostr 加密后的结果需要和微信的结果一致
+     * @return 校验是否正确
+     */
     boolean check(String timestamp, String nonce, String signature, String echostr);
 
+    /**
+     * 解析微信发送的 inputStream 为 Map
+     * @param context 请求体
+     * @return Map<String, String> map
+     */
     Map<String, String> parseRequestInputStream(String context);
 
     /**
