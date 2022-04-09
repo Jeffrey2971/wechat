23aa901133f362a54b09999e46cdc12225f8edef
Jeffrey <Jeffrey2971@outlook.com>
2022-04-10 00:24:23 +0800

    WeChat：用户使用量拦截器

--- src/main/java/com/jeffrey/wechat/interceptor/TotalInterceptor.java
+++ src/main/java/com/jeffrey/wechat/interceptor/TotalInterceptor.java
@@ -1,30 +1,57 @@
 package com.jeffrey.wechat.interceptor;
 
+import com.jeffrey.wechat.entity.mybatis.UserUseTotalEntity;
+import com.jeffrey.wechat.service.GetFreeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 
 /**
+ *
+ *
  * @author jeffrey
  * @since JDK 1.8
  */
 
-
+@Component
 public class TotalInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        return HandlerInterceptor.super.preHandle(request, response, handler);
+
+    private final GetFreeService getFreeService;
+
+    @Autowired
+    public TotalInterceptor(GetFreeService getFreeService) {
+        this.getFreeService = getFreeService;
     }
 
+    /**
+     * 如果用户发送的消息类型为 image 图片类型的话，在今日可使用次数的基础上减一
+     */
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
-    }
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+        if (request.getAttribute("requestMap") instanceof HashMap) {
+            HashMap<?, ?> requestMap = (HashMap<?, ?>) request.getAttribute("requestMap");
+            String oid = (String) requestMap.get("FromUserName");
+            if (StringUtils.hasText(oid)) {
+                if ("image".equalsIgnoreCase((String) requestMap.get("MsgType"))) {
+
+                    UserUseTotalEntity userUseTotalEntity = getFreeService.getUserUseTotalTableEntityByOpenId(oid);
+
+                    // 如果结果不为 0 或 不为 null，在基础上 -1
+                    Integer canUse = userUseTotalEntity.getCanUse();
+
+                    // 在基础上 +1
+                    Integer allUse = userUseTotalEntity.getAllUse();
+
+                    if ("F".equalsIgnoreCase(String.valueOf(userUseTotalEntity.getFreeUser())) && canUse != null && canUse != 0) {
+                        getFreeService.updateUserTotal(--canUse, userUseTotalEntity.getFreeUser(), ++allUse, userUseTotalEntity.getFree(), oid);
+                    }
+                }
+            }
+        }
     }
 }
