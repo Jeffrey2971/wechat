857c3cef952e385f8d790277a6257b391ad7f975
Jeffrey <Jeffrey2971@outlook.com>
2022-04-10 15:10:50 +0800

    WeChat：微信可上传素材 mediaType

---
+++ src/main/java/com/jeffrey/wechat/entity/enumeration/WxMediaType.java
@@ -0,0 +1,42 @@
+package com.jeffrey.wechat.entity.enumeration;
+
+/**
+ * @author jeffrey
+ * @since JDK 1.8
+ */
+
+
+public class WxMediaType {
+
+    /**
+     *
+     */
+    public static final String APPLICATION_OCTET_STREAM = "application/octet-stream";
+
+    /**
+     * 可上传的图片
+     */
+    public static final String IMAGE_JPEG = "image/jpeg";
+    public static final String IMAGE_PNG = "image/png";
+    public static final String IMAGE_BMP = "image/bmp";
+    public static final String IMAGE_GIF = "image/gif";
+
+    /**
+     * 可上传的音频
+     */
+    public static final String AUDIO_MP3 = "audio/mpeg";
+    public static final String AUDIO_WAV = "audio/x-wav";
+    public static final String AUDIO_WMA = "audio/x-ms-wma";
+    public static final String AUDIO_AMR = "audio/amr";
+
+    /**
+     * 可上传的视频
+     */
+    public static final String VIDEO_MP4 = "video/mp4";
+
+    /**
+     * 可上传的缩略图
+     */
+    public static final String THUMB = "image/jpg";
+
+}
