ad4b72325fc595804ac7271140bc9c865d06017b
Jeffrey <Jeffrey2971@outlook.com>
2022-04-17 03:23:37 +0800

    WeChat：微信消息类型以及事件类型枚举类

---
+++ src/main/java/com/jeffrey/wechat/entity/enumeration/WxEventType.java
@@ -0,0 +1,15 @@
+package com.jeffrey.wechat.entity.enumeration;
+
+public enum WxEventType {
+    subscribe,
+    unsubscribe,
+    click,
+    view,
+    pic_photo_or_album,
+    scancode_push,
+    scancode_waitmsg,
+    pic_sysphoto,
+    pic_weixin,
+    location_select,
+    view_miniprogram,
+}
---
+++ src/main/java/com/jeffrey/wechat/entity/enumeration/WxMessageType.java
@@ -0,0 +1,12 @@
+package com.jeffrey.wechat.entity.enumeration;
+
+public enum WxMessageType {
+    event,
+    text,
+    image,
+    voice,
+    shortvideo,
+    video,
+    location,
+    link
+}
