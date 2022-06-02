63b9979e7a3be7fa826e7c78d23aee33bbbebede
Jeffrey <Jeffrey2971@outlook.com>
2022-06-03 01:00:56 +0800

    WeChat：封装微信模板消息发送的所需信息的实体类

---
+++ src/main/java/com/jeffrey/wechat/entity/translation/DocTranslation.java
@@ -0,0 +1,23 @@
+package com.jeffrey.wechat.entity.translation;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 封装了微信模板消息发送的所需信息
+ *
+ * @author jeffrey
+ * @since JDK 1.8
+ */
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DocTranslation {
+    private String filename;
+    private String openid;
+    private String langFrom;
+    private String langTo;
+    private long timestamp;
+}
