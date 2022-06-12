dde6768bf2c100efdca0c094436730678f971a45
Jeffrey <Jeffrey2971@outlook.com>
2022-06-12 22:36:18 +0800

    WeChat：配置静态资源的缓存时间为一年，文档资源为半年

--- src/main/java/com/jeffrey/wechat/config/WeChatWebConfiguration.java
+++ src/main/java/com/jeffrey/wechat/config/WeChatWebConfiguration.java
@@ -7,10 +7,12 @@ import com.jeffrey.wechat.interceptor.UserRequestInterceptor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.util.UrlPathHelper;
 
 import java.io.File;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author jeffrey
@@ -29,8 +31,15 @@ public class WeChatWebConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/doc/**").addResourceLocations(String.format("file:%s/", new File(WechatApplication.documentResPath, "/doc")));
-        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/doc/**").addResourceLocations(String.format("file:%s/", new File(WechatApplication.documentResPath, "/doc")))
+                .setCacheControl(CacheControl.maxAge(183, TimeUnit.DAYS)
+                .cachePrivate())
+                .setCachePeriod(183 * (24  * 60 * 60));
+
+        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS)
+                .cachePublic())
+                .setCachePeriod(365 * (24 * 60 * 60));
     }
 
     @Autowired
