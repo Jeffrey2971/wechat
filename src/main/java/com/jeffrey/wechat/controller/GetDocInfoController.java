ec679d5c8976c116bb693281865e948ef399ad27
Jeffrey <Jeffrey2971@outlook.com>
2022-06-27 18:57:51 +0800

    WeChat：添加缓存 Expires 字段，以兼容 HTTP 1.0

--- src/main/java/com/jeffrey/wechat/controller/GetDocInfoController.java
+++ src/main/java/com/jeffrey/wechat/controller/GetDocInfoController.java
@@ -2,15 +2,18 @@ package com.jeffrey.wechat.controller;
 
 import com.jeffrey.wechat.entity.TransResponseWrapper;
 import com.jeffrey.wechat.service.GetDocInfoService;
+import com.jeffrey.wechat.utils.SaveAndReadImageDocument;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.HashMap;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Locale;
+import java.util.TimeZone;
 
 /**
  * @author jeffrey
@@ -23,32 +26,33 @@ public class GetDocInfoController {
 
     private final GetDocInfoService docInfoService;
 
-    private final HashMap<Long, TransResponseWrapper> userDataItem;
-
     @Autowired
-    public GetDocInfoController(GetDocInfoService docInfoService, HashMap<Long, TransResponseWrapper> userDataItem) {
+    public GetDocInfoController(GetDocInfoService docInfoService) {
         this.docInfoService = docInfoService;
-        this.userDataItem = userDataItem;
     }
 
     @GetMapping(value = "/info/{id}", produces = "text/html;charset=UTF-8")
     public String info(
             @PathVariable(required = false) Integer id,
-            @MatrixVariable(required = false) Long wrapper,
+            @MatrixVariable(required = false) String wrapper,
             @MatrixVariable(required = false) String openid,
             Model model,
             HttpServletRequest request,
             HttpServletResponse response
     ){
-        //--------------- CEP Servlet3 ---------------//
+        //--------------- 添加缓存 ---------------//
 
         request.setAttribute("openid", openid);
-        response.setHeader("Cache-Control", "max-age=1800");
+        response.setHeader("Cache-Control", "public, max-age=2626560");
+        DateFormat df = new SimpleDateFormat("EEE, dd MMM yyyy hh:mm:ss 'GMT'", Locale.US);
+        df.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String expires = df.format(System.currentTimeMillis() + (30L * 24 * 60 * 60 * 1000));
+        response.setHeader("expires", expires);
 
-        if (userDataItem.containsKey(wrapper) && id > 0 && id <= 5) {
-            TransResponseWrapper item = userDataItem.get(wrapper);
+        if (SaveAndReadImageDocument.containsKey(wrapper) && id > 0 && id <= 5) {
+            TransResponseWrapper item = SaveAndReadImageDocument.deSerialJsonToClass(wrapper, TransResponseWrapper.class);
             if (openid.equals(item.getOpenid())) {
-                return docInfoService.prepareData(id, wrapper, model, userDataItem);
+                return docInfoService.prepareData(id, wrapper, model);
             } else {
                 model.addAttribute("title", "请先关注");
                 model.addAttribute("label1", "401 Unauthorized");
