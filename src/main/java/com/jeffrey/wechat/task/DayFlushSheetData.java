baa2e1dcbeed9d9830565036af677c8a34e3ac99
Jeffrey <Jeffrey2971@outlook.com>
2022-06-27 18:57:20 +0800

    WeChat：新增任务计划，每日凌晨三点移除前一天取消关注用户的 redis 数据

--- src/main/java/com/jeffrey/wechat/task/DayFlushSheetData.java
+++ src/main/java/com/jeffrey/wechat/task/DayFlushSheetData.java
@@ -1,14 +1,15 @@
 package com.jeffrey.wechat.task;
 
 import com.jeffrey.wechat.config.WeChatAutoConfiguration;
+import com.jeffrey.wechat.entity.TransResponseWrapper;
 import com.jeffrey.wechat.service.GetFreeService;
 import com.jeffrey.wechat.service.WeChatService;
+import com.jeffrey.wechat.utils.SaveAndReadImageDocument;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
 import java.util.List;
 
 /**
@@ -37,8 +38,10 @@ public class DayFlushSheetData implements InitializingBean {
     public void afterPropertiesSet() {
         this.flushShareTable();
         this.flushUserUseTotalTable();
+        this.flushRedisNotFollowUserData();
     }
 
+    //--------------------移除 share 表中无效数据--------------------//
     @Scheduled(cron = "0 0 0 * * ?")
     public void flushShareTable() {
 
@@ -56,10 +59,12 @@ public class DayFlushSheetData implements InitializingBean {
 
     }
 
+    //--------------------刷新 user_use_total 表内用户的每日免费试用量--------------------//
     @Scheduled(cron = "0 0 0 * * ?")
     public void flushUserUseTotalTable() {
 
         log.info("重置用户每日使用量");
+
         getFreeService.resetCanUseColumn(wxConfig.getWxDayCanUse());
 
         log.info("移除表中无效用户");
@@ -75,6 +80,33 @@ public class DayFlushSheetData implements InitializingBean {
 
             log.info("成功移除了 {} 位用户", getFreeService.deleteListUser(GFSUserOpenIdList));
         }
+    }
+
+    //----------------------------移除 redis 中无效数据---------------------------//
+    @Scheduled(cron = "0 0 3 * * ?")
+    public void flushRedisNotFollowUserData(){
+
+        log.info("移除 redis 中无效数据");
+
+        List<TransResponseWrapper> entities = SaveAndReadImageDocument.getEntities();
+
+        if (entities == null || entities.size() == 0) {
+            return;
+        }
+
+        for (TransResponseWrapper entity : entities) {
+            if (entity == null) {
+                continue;
+            }
+            List<String> openIdList = weChatService.selectUserOpenIdList();
+            if (!openIdList.contains(entity.getOpenid())) {
+
+                //----------反推过期时间得到key值----------//
+                String key = String.valueOf(entity.getExpiredTimeStamp() - (30L * 24 * 60 * 60 * 1000));
+                SaveAndReadImageDocument.removeKey(key);
+            }
+        }
+
 
     }
 }
