12a032da92e37f916cf1e6fba0bf4798e8e57014
Jeffrey <Jeffrey2971@outlook.com>
2022-07-04 16:12:59 +0800

    WeChat：修改返回的 List 为 Map，Map 的 key 为 redis 中对应的键，value 不变

--- src/main/java/com/jeffrey/wechat/utils/SaveAndReadImageDocument.java
+++ src/main/java/com/jeffrey/wechat/utils/SaveAndReadImageDocument.java
@@ -7,9 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -25,17 +23,18 @@ public class SaveAndReadImageDocument {
     public static RedisTemplate<String, String> redisTemplate;
 
     @Autowired
-    public void setRedisTemplate(RedisTemplate<String, String> redisTemplate){
+    public void setRedisTemplate(RedisTemplate<String, String> redisTemplate) {
         SaveAndReadImageDocument.redisTemplate = redisTemplate;
     }
 
     /**
      * 将给定的的对象转为 json 并以 string 类型的方式存入 redis 中
+     *
      * @param wrapperKey string key
-     * @param wrapper string value
+     * @param wrapper    string value
      * @return 保存是否成功
      */
-    public static boolean serialToJsonAndSave(String wrapperKey, TransResponseWrapper wrapper){
+    public static boolean serialToJsonAndSave(String wrapperKey, TransResponseWrapper wrapper) {
 
         if (StringUtils.isBlank(wrapperKey) || wrapper == null) {
             log.error("传入的 wrapperKey 为 0 或 wrapper 为空");
@@ -61,8 +60,9 @@ public class SaveAndReadImageDocument {
 
     /**
      * 根据给定的 wrapperKey 获取 redis 中的 value 并转为 TransResponseWrapper 返回
+     *
      * @param wrapperKey 查找的 key
-     * @param wrapper TransResponseWrapper.class
+     * @param wrapper    TransResponseWrapper.class
      * @return TransResponseWrapper
      */
     public static TransResponseWrapper deSerialJsonToClass(String wrapperKey, Class<TransResponseWrapper> wrapper) {
@@ -85,21 +85,23 @@ public class SaveAndReadImageDocument {
      * @param key 给定的 key
      * @return 不为 null 且为 true
      */
-    public static boolean containsKey(String key){
+    public static boolean containsKey(String key) {
         Boolean result = redisTemplate.hasKey(key);
         return result != null && result;
     }
 
     /**
      * 移除 redis 中的 key
+     *
      * @param key 要删除的 key
      */
-    public static void removeKey(String key){
+    public static void removeKey(String key) {
         Boolean result = redisTemplate.delete(key);
-        if (result == null || !result) {
-            log.error("删除 {} 失败", key);
+        if (result != null && result) {
+            log.info("移除 redis 无效数据：{}", key);
+        } else {
+            log.error("移除 redis 无效数据失败：{}", key);
         }
-        log.info("移除：{}", key);
     }
 
     /**
@@ -108,16 +110,17 @@ public class SaveAndReadImageDocument {
      * @param key 给定的 key
      * @return 获取过期时间，-2 表示已过期
      */
-    public static long getKeyExpireTime(String key){
+    public static long getKeyExpireTime(String key) {
         Long expire = redisTemplate.getExpire(key, TimeUnit.DAYS);
         return expire == null ? -2 : expire;
     }
 
     /**
      * 获取 redis 中的所有文档数据并封装为 List<TransResponseWrapper> 返回
+     *
      * @return List<TransResponseWrapper>
      */
-    public static List<TransResponseWrapper> getEntities(){
+    public static Map<String, TransResponseWrapper> getEntities() {
 
         Set<String> keys = redisTemplate.keys("*");
 
@@ -125,21 +128,11 @@ public class SaveAndReadImageDocument {
             return null;
         }
 
-        List<String> values = redisTemplate.opsForValue().multiGet(keys);
+        HashMap<String, TransResponseWrapper> documentMap = new HashMap<>();
 
-        if (values == null || values.size() == 0) {
-            return null;
-        }
+        keys.forEach(item -> documentMap.put(item, new Gson().fromJson(redisTemplate.opsForValue().get(item), TransResponseWrapper.class)));
 
-        ArrayList<TransResponseWrapper> transResponseWrappers = new ArrayList<>();
-
-        for (String value : values) {
-            TransResponseWrapper transResponseWrapper = new Gson().fromJson(value, TransResponseWrapper.class);
-            transResponseWrappers.add(transResponseWrapper);
-        }
-
-        return transResponseWrappers;
+        return documentMap;
 
     }
-
 }
