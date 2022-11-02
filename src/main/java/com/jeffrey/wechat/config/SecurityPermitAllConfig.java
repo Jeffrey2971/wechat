be9c82f31fad51952e5b4000efe3324e63fb6d65
Jeffrey <Jeffrey2971@outlook.com>
2022-11-02 22:49:04 +0800

    WeChat：        1、引入 security 安全模块后会因为安全策略导致矩阵变量被拦截失效，设置 security 防火墙允许矩阵分号。解决获取文档失败的问题

---
+++ src/main/java/com/jeffrey/wechat/config/SecurityPermitAllConfig.java
@@ -0,0 +1,34 @@
+package com.jeffrey.wechat.config;
+
+/**
+ * @author jeffrey
+ * @since JDK 1.8
+ */
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.firewall.HttpFirewall;
+import org.springframework.security.web.firewall.StrictHttpFirewall;
+
+@Configuration
+public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().anyRequest().permitAll()
+                .and().csrf().disable();
+    }
+
+    /**
+     * 引入 security 安全模块后会因为安全策略导致矩阵变量被拦截失效，设置 security 防火墙允许矩阵分号
+     * @return HttpFirewall
+     */
+    @Bean
+    public HttpFirewall allowUrlSemicolonHttpFirewall() {
+        StrictHttpFirewall firewall = new StrictHttpFirewall();
+        firewall.setAllowSemicolon(true);
+        return firewall;
+    }
+}
